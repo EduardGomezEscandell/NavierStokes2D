22c76d820656437129ae53559ab65ea84837bc72
Eduard Gómez <eduard.gomez.escandell@gmail.com>
2020-05-09 19:49:20 +0200

    Changes made during saturday call

--- non_iterated_arrays.m
+++ non_iterated_arrays.m
@@ -1,16 +1,11 @@
-function [K, K1, K21, K22, C1, C21, C22, M] = FEM_matrices(coords, refelem, X, dX, visc)
+function [K, K1, C1, M, b] = non_iterated_arrays(coords, X, dX, visc, refelem)
     nodes_per_elem = length(coords);
     
     K = zeros(nodes_per_elem);
     K1 = zeros(nodes_per_elem);
-    K21 = zeros(nodes_per_elem);
-    K22 = zeros(nodes_per_elem);
-    
     C1 = zeros(nodes_per_elem);
-    C21 = zeros(nodes_per_elem);
-    C22 = zeros(nodes_per_elem);
-        
     M = zeros(nodes_per_elem);
+    b = zeros(nodes_per_elem);
     jacobian = refelem.jacobian(coords);
     
     if refelem.shape=='Q' % Quads
@@ -25,22 +20,9 @@ function [K, K1, K21, K22, C1, C21, C22, M] = FEM_matrices(coords, refelem, X, d
                 gradN = refelem.gradN(:,:,p);
                 w = 1; % placeholder
                 
-                % Gradients
-                grad_dRho= invJ * gradN * X(:,3);
-                grad_du = invJ * gradN * dX(:,1);
-                grad_dv = invJ * gradN * dX(:,2); 
-                
-                % Stiffnes
                 K = K + w * (invJ*gradN)' * invJ*gradN;
-                
                 K1  = K1  + w * (invJ*gradN)' * (N*visc) * invJ*gradN;  % K1(nu)
-                K21 = K21 + w * (invJ*gradN)' * grad_du * N;             % K2(u)
-                K22 = K22 + w * (invJ*gradN)' * grad_dv * N;             % K2(v)
-                
                 C1  = C1  + w * N' * ((N*X(:,1:2) * invJ*gradN));  % C1(u,v)
-                C21 = C21 + w * (N' * N) * grad_dRho(1);             % C21(rho)
-                C21 = C21 + w * (N' * N) * grad_dRho(2);             % C22(rho)
-                
                 M = M + w * (N'*N);
                 p = p+1;
             end
@@ -48,12 +30,6 @@ function [K, K1, K21, K22, C1, C21, C22, M] = FEM_matrices(coords, refelem, X, d
         
         detJ = det(jacobian.calc(jacobian,0,0));
         K   = K   * detJ;
-        K1  = K1  * detJ;
-        K21 = K21 * detJ;
-        K22 = K22 * detJ;
-        C1  = C1  * detJ;
-        C21 = C21 * detJ;
-        C22 = C22 * detJ;
         M   = M   * detJ;
         
     else
---
+++ post_processing_single.m
@@ -0,0 +1,21 @@
+function post_processing_single(coords, X)
+    n_nodes = size(coords,2);
+    
+    width = max(coords(1,:));
+    height = max(coords(2,:));
+
+    T = delaunay(coords(1,:), coords(2,:));
+
+    t = trisurf(T, coords(1,:)', coords(2,:)', zeros(n_nodes,1), ...
+           X(2*n_nodes+1:3*n_nodes));
+    t.EdgeColor = 'None';
+    shading interp
+    hold on
+    quiver(coords(1,:)',                ...
+           coords(2,:)',                ...
+           X(1:n_nodes),   ...
+           X(n_nodes+1:2*n_nodes),'r');
+    hold off
+    view(2)
+    axis([-1 width+1 -1 height+1]);
+end
